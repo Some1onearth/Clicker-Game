f8ee9e98b835069be034fbd79f2c6a57e475b89a
Some1onearth <chris_lee@outlook.com.au>
2021-09-09 15:59:58 +1000

    a bit more work

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,13 +6,20 @@ using UnityEngine.UI;
 public class ButtonManager : MonoBehaviour
 {
     public static int conspiracySpread;
-    [SerializeField]
-    Text conspiracyUICounter;
+    public static int research;
+    public static int avoid5G;
+    public static int PostTwit;
+    public static int uploadVideo;
+    public static int stream;
+    [SerializeField] Text conspiracyUICounter;
+    [SerializeField] Text researchUICounter;
     // Start is called before the first frame update
     void Start()
     {
         conspiracySpread = PlayerPrefs.GetInt("conspiracies", 0); //loads in saved clicks
-        UpdateText();
+        UpdateConspiracyText();
+        research = PlayerPrefs.GetInt("research", 0);
+        UpdateResearchText();
     }
 
     public void BigButtonPress() //can't have several paramaters (ie. adding "int i" after "string message" or Nukes launched
@@ -20,11 +27,20 @@ public class ButtonManager : MonoBehaviour
         conspiracySpread += 1;
         Debug.Log(conspiracySpread + " Conspiracy Spread"); //saves clicks
 
-        UpdateText();
+        UpdateConspiracyText();
 
         PlayerPrefs.SetInt("conspiracies", conspiracySpread);
     }
-    void UpdateText()
+    public void ResearchButtonPress()
+    {
+        research += 1;
+        Debug.Log(research + "You've done your research"); //saves clicks
+
+        UpdateResearchText();
+
+        PlayerPrefs.SetInt("research", research);
+    }
+    void UpdateConspiracyText()
     {
         if(conspiracySpread != 1)
         {
@@ -35,4 +51,17 @@ public class ButtonManager : MonoBehaviour
             conspiracyUICounter.text = conspiracySpread + " \n<size=50>Conspiracy Spread</size>";
         }
     }
+    void UpdateResearchText()
+    {
+        if (research != 1)
+        {
+            researchUICounter.text = research + " \n<size=48>Do your research</size>";
+        }
+        else
+        {
+            researchUICounter.text = research + " \n<size=48>Do your research</size>";
+        }
+    }
+
+
 }
